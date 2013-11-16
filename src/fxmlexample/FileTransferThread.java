d8e94d6b2f08dc01ded8d58b0d932652a755b670
liangrubo <liangrubo@gmail.com>
2013-11-16 15:18:46 +0800

    auto connect to make it resistent to network status change

--- src/fxmlexample/FileTransferThread.java
+++ src/fxmlexample/FileTransferThread.java
@@ -14,6 +14,13 @@ import com.jcraft.jsch.SftpException;
 
 public class FileTransferThread extends Thread {
 
+	static {
+		// static initializer for JSch
+		JSch.setConfig("StrictHostKeyChecking", "no");
+		JSch.setConfig("PreferredAuthentications",
+				"publickey,gssapi-with-mic,keyboard-interactive,password");
+	}
+
 	private final String remote;
 	private final String local;
 	private final String host;
@@ -42,37 +49,90 @@ public class FileTransferThread extends Thread {
 
 	@Override
 	public void run() {
-		JSch.setConfig("StrictHostKeyChecking", "no");
+		while (!this.isInterrupted()) {
+			try {
+				this._run();
+				this.controller.addLog("暂停同步, 30秒后重新尝试");
+				Thread.sleep(30000);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+		this.controller.reset();
+		this.controller.addLog("同步已退出，请重新同步");
+		this.controller.showMessage("同步已退出", "同步已退出",
+				MessageBox.ICON_INFORMATION | MessageBox.OK);
+	}
 
-		final JSch jsch = new JSch();
+	private ChannelSftp getChannel() throws InterruptedException, JSchException {
+		JSch jsch = new JSch();
 		ChannelSftp channel = null;
 		Session session = null;
-		try {
-
-			session = jsch.getSession(this.username, this.host, this.port);
-			session.setConfig("PreferredAuthentications", "publickey,gssapi-with-mic,keyboard-interactive,password");
-
-			if (this.key != null && this.key.length() > 0) {
-				if (this.password != null && this.password.length() > 0) {
-					jsch.addIdentity(this.key, this.password);
-					this.controller.addLog("使用有密码密钥登陆");
-				} else {
-					jsch.addIdentity(this.key);
-					this.controller.addLog("使用密钥登陆");
-				}
+		session = jsch.getSession(this.username, this.host, this.port);
+		if (this.key != null && this.key.length() > 0) {
+			if (this.password != null && this.password.length() > 0) {
+				jsch.addIdentity(this.key, this.password);
+				this.controller.addLog("使用有密码密钥登陆");
 			} else {
-				session.setPassword(this.password);
-				this.controller.addLog("使用密码登陆");
+				jsch.addIdentity(this.key);
+				this.controller.addLog("使用密钥登陆");
 			}
+		} else {
+			session.setPassword(this.password);
+			this.controller.addLog("使用密码登陆");
+		}
 
-			session.connect();
+		// 尝试连接三次
+		boolean isConnected = false;
+		JSchException connectException = null;
+		for (int i = 0; i < 3; i++) {
+			try {
+				session.connect();
+				isConnected = true;
+				break;
+			} catch (JSchException e) {
+				connectException = e;
+				Thread.sleep(5000);
+			}
+		}
+		if (!isConnected) {
+			throw connectException;
+		}
 
-			channel = (ChannelSftp) session.openChannel("sftp");
-			channel.connect();
+		channel = (ChannelSftp) session.openChannel("sftp");
+		channel.connect();
 
-			session.setConfig("compression.s2c", "zlib@openssh.com,zlib,none");
-			session.setConfig("compression.c2s", "zlib@openssh.com,zlib,none");
+		session.setConfig("compression.s2c", "zlib@openssh.com,zlib,none");
+		session.setConfig("compression.c2s", "zlib@openssh.com,zlib,none");
+		return channel;
+	}
 
+	private void disconnect(ChannelSftp channel) {
+		if (channel == null) {
+			return;
+		}
+		channel.disconnect();
+		try {
+			channel.getSession().disconnect();
+		} catch (JSchException e) {
+			// ignore intentionally
+		}
+	}
+
+	private void _run() throws InterruptedException {
+		if (this.directoryCreateEvents.size() > 100) {
+			this.controller.showMessage("有" + this.directoryCreateEvents.size()
+					+ "个文件夹创建事件没有同步了, 再不处理可能内存要爆了!", "同步警告",
+					MessageBox.ICON_WARNING | MessageBox.OK);
+		}
+		if (this.fileEvents.size() > 1000) {
+			this.controller.showMessage("有" + this.fileEvents.size()
+					+ "个文件事件没有同步了, 再不处理可能内存要爆了!", "同步警告",
+					MessageBox.ICON_WARNING | MessageBox.OK);
+		}
+		ChannelSftp channel = null;
+		try {
+			channel = this.getChannel();
 			channel.cd(this.remote);
 			channel.lcd(this.local);
 
@@ -82,41 +142,33 @@ public class FileTransferThread extends Thread {
 
 			while (!this.isInterrupted()) {
 				synchronized (this) {
-					for(FileEvent event: directoryCreateEvents){
+					for (FileEvent event : directoryCreateEvents) {
 						String filename = event.filename;
 						channel.mkdir(filename);
 						this.controller.addLog("创建文件夹: " + filename + " ok");
 					}
 					this.directoryCreateEvents.clear();
-					
+
 					for (final FileEvent event : this.fileEvents) {
 						String filename = event.filename;
-						channel.put(filename, filename.replace("\\", "/"), null,
-								ChannelSftp.OVERWRITE);
+						channel.put(filename, filename.replace("\\", "/"),
+								null, ChannelSftp.OVERWRITE);
 						this.controller.addLog("上传文件: " + filename + " ok");
 					}
 					this.fileEvents.clear();
 					this.wait();
 				}
-
 			}
-		} catch (final JSchException | SftpException | InterruptedException e) {
-			this.controller.reset();
-			this.controller.addLog("同步发生错误，错误代码：" + e.getMessage());
+		} catch (SftpException e) {
+			this.controller.addLog("同步异常: " + e.getMessage());
+		} catch (InterruptedException e) {
+			this.controller.addLog("收到停止信号, 准备退出");
+			throw e;
+		} catch (JSchException e) {
+			this.controller.addLog("连接异常: " + e.getMessage());
 		} finally {
-			if (channel != null) {
-				channel.disconnect();
-			}
-
-			if (session != null) {
-				session.disconnect();
-			}
+			this.disconnect(channel);
 		}
-
-		this.controller.addLog("同步已退出，请重新同步");
-		this.controller.showMessage("同步已退出", "同步已退出",
-				MessageBox.ICON_INFORMATION | MessageBox.OK);
-
 	}
 
 	public void addFileEvent(FileEvent event) {
